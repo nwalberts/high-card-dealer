6ce5954208a01dfae91194dfe637f68ef2b1fc5c
Nick Alberts <nwalberts5@gmail.com>
2018-06-12 11:54:40 -0400

    add error noteS

---
+++ notes.rb
@@ -0,0 +1,11 @@
+# error 1: unexpected keyword end
+# error 2: uninitialized constant Deck - require_relative, open pry for explaning referring to undefined classes
+# error 1: `initialize': wrong number of arguments for Deck - ask how we approach, explain instance variable even without argument in initialize
+# error 1: uninitialized constant Deck::SUIT - pluralization matters
+# error 1: uninitialized constant Deck::Card - require relative
+# error 1:  undefined local variable or method `suit' - add a pry in the loop to inspect the variable for suit, nested looping and |block arguments|
+# error 1: deck.rb:16:in `initialize': wrong number of arguments - define card class (minimally)
+# error 1: undefined method `deal' for Deck:Class (NoMethodError) - not an class method!
+# error 1: wrong number of arguments:
+# error 1: uninitialized constant Hand (NameError) - require hand and define hand
+# error 1: undefined method `card_output' for #<Hand:0x007f913903b610 @cards=nil> (NoMethodError)
